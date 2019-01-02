d1e6395a97dcfbfb4c1b735cc88ee1f4fdbe91bf
kumu1997 <43980319+kumu1997@users.noreply.github.com>
2019-01-02 11:06:13 +0530

    Add files via upload

---
+++ Class18.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringcharactersidentification
+{
+    class Class18
+    {
+        public static void Main19(string [] args)
+        {
+            string s = Console.ReadLine();
+            int number=0,capital = 0, small = 0, special = 0, space = 0;
+            char[] a = s.ToCharArray();
+            for(int i=0;i<a.Length;i++)
+            {
+                if (a[i] >= 48 && a[i] <= 58)
+                    number += 1;
+                else if (a[i] >= 65 && a[i] <= 90)
+                    capital += 1;
+                else if (a[i] >= 97 && a[i] <= 122)
+                    small += 1;
+                else if (a[i] == 32)
+                    space += 1;
+                else
+                    special += 1;
+            }
+            Console.WriteLine("Numbers are               "  + number);
+            Console.WriteLine("Capital Characters are    " + capital);
+            Console.WriteLine("Small Characters are      " + small);
+            Console.WriteLine("Space Characters are      " + space);
+            Console.WriteLine("Special Characters are    " + special);
+
+        }
+    }
+}
---
+++ Class19.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Removeduplicatestringcharacters
+{
+    class Class19
+    {
+        public static void Main20(string [] args)
+        {
+            String value1 = Removeduplicatechars("Google");
+            Console.WriteLine(value1);
+        }
+
+        static string Removeduplicatechars(string key)
+        {
+            string temp = "";
+            string result = "";
+
+            foreach(char value in key)
+            {
+                if(temp.IndexOf(value)==-1)
+                {
+                    temp += value;
+                    result += value;
+                }
+            }
+            return result;
+        }
+    }
+}
