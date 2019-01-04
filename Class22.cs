d9207f97417b088c2e60da6a9fc193b0e37f082b
kumu1997 <43980319+kumu1997@users.noreply.github.com>
2019-01-04 11:40:15 +0530

    Add files via upload

---
+++ Class22.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printduplicatechars
+{
+    class Class22
+    {
+        public static void Main(string [] args)
+        {
+            string strInput = Console.ReadLine();
+          
+                var dict = new Dictionary<char, int>();
+                foreach (var item in strInput)
+                {
+                    if (dict.ContainsKey(item))
+                    {
+                        dict[item]++;
+                    }
+                    else
+                    {
+                        dict.Add(item, 1);
+                    }
+                }                
+                foreach (var item in dict)
+                {
+                    if (item.Value > 1)
+                    {
+                        Console.WriteLine(item.Key);
+                    }
+                }                        
+
+        }
+       
+    }
+}
